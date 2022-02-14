ec06f6907730218cce4813293ad2cd118909861e
dbocar12 <33195239+dbocar12@users.noreply.github.com>
2022-02-14 18:10:00 +0100

    Adding data in Player Table in db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO team (name, slogan) VALUES ('Real Madrid', 'Les galactiques');
 INSERT INTO team (name, slogan) VALUES ('Barcelone', 'La Macia');
 INSERT INTO team (name, slogan) VALUES ('Bayern', 'Les puissants en Allemagne');
 INSERT INTO team (name, slogan) VALUES ('Manchester United', 'Les red devils');
+
+insert into Player (id, name, number, position,team_id) values(1,'Lionel Messi','10','Forward',1);
+insert into Player (id, name, number, position,team_id) values(2,'Karim Benzema','9','Forward',2);
+insert into Player (id, name, number, position,team_id) values(3,'Gerald Pique','3','Defense',3);
+insert into Player (id, name, number, position,team_id) values(4,'Manuel Neuer','1','Goalkeeper',4);
+insert into Player (id, name, number, position,team_id) values(5,'Cristiano Ronaldo','7','Forward',5);
